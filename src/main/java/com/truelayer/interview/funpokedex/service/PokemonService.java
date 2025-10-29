ac273b7f19fbfc7f93f7fd8737345412b6ef314b
mell0r1ne <58421605+mell0r1ne@users.noreply.github.com>
2025-10-29 23:59:11 +0100

    add methods to retrieve Pokémon info and translate descriptions

--- src/main/java/com/truelayer/interview/funpokedex/service/PokemonService.java
+++ src/main/java/com/truelayer/interview/funpokedex/service/PokemonService.java
@@ -1,6 +1,9 @@
 package com.truelayer.interview.funpokedex.service;
 
-import com.truelayer.interview.funpokedex.client.PokemonClient;
+import com.truelayer.interview.funpokedex.client.pokemon.PokemonClient;
+import com.truelayer.interview.funpokedex.model.PokemonResponse;
+import com.truelayer.interview.funpokedex.model.client.pokemon.FlavorTextEntry;
+import com.truelayer.interview.funpokedex.model.client.pokemon.PokemonApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +13,26 @@ public class PokemonService {
     private final PokemonClient pokemonClient;
     private final TranslationService translationService;
 
+
+    public PokemonResponse getPokemonInfo(String name) {
+        PokemonApiResponse apiResponse = pokemonClient.getPokemonByName(name);
+        return PokemonResponse.builder()
+                .name(apiResponse.getName())
+                .description(
+                    apiResponse.getFlavorTextEntries().stream()
+                        .filter(t -> t.getLanguage() != null && "en".equals(t.getLanguage().getName()))
+                        .findFirst()
+                        .map(FlavorTextEntry::getFlavorText)
+                        .orElse("")
+                )
+                .habitat(apiResponse.getHabitat() != null ? apiResponse.getHabitat().getName() : "")                .habitat(apiResponse.getHabitat() != null ? apiResponse.getHabitat().getName() : "")
+                .isLegendary(apiResponse.isLegendary())
+                .build();
+    }
+
+    public PokemonResponse getTranslatedPokemonInfo(String name) {
+        PokemonResponse pokemon = this.getPokemonInfo(name);
+
+        return translationService.translate(pokemon);
+    }
 }
--- src/main/java/com/truelayer/interview/funpokedex/service/TranslationService.java
+++ src/main/java/com/truelayer/interview/funpokedex/service/TranslationService.java
@@ -1,7 +1,8 @@
 package com.truelayer.interview.funpokedex.service;
 
 
-import com.truelayer.interview.funpokedex.client.TranslationClient;
+import com.truelayer.interview.funpokedex.client.translator.TranslationClient;
+import com.truelayer.interview.funpokedex.model.PokemonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +11,20 @@ import org.springframework.stereotype.Service;
 public class TranslationService {
     private final TranslationClient translationClient;
 
+    public PokemonResponse translate(PokemonResponse pokemon) {
+        String authorName = (pokemon.getHabitat() != null && pokemon.getHabitat().equalsIgnoreCase("cave")) || pokemon.isLegendary()
+                ? "yoda"
+                : "shakespeare";
+
+        String translatedText = translationClient.getTranslation(authorName, pokemon.getDescription())
+                .getContent()
+                .getTranslated();
+
+        return PokemonResponse.builder()
+                .name(pokemon.getName())
+                .description(translatedText)
+                .habitat(pokemon.getHabitat())
+                .isLegendary(pokemon.isLegendary())
+                .build();
+    }
 }
